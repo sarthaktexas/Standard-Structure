a10ef6c71398101ed0e675058e56b840358369fe
Matthew Gleich <matthewgleich@gmail.com>
2020-05-24 22:04:59 -0400

    🚀 Finalized CLI
    Signed-off-by: Matthew Gleich <matthewgleich@gmail.com>

--- struct-add/struct-add/__main__.py
+++ struct-add/struct-add/__main__.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 
 import ask
 import readme
@@ -12,6 +13,12 @@ def main():
     if not os.path.exists('../README.md'):
         readme.initializeMain()
     readme.add_to_toc(location.split('/')[-2])
+    treeCommand = readme.tree(Path.cwd())
+    treeLines = []
+    for line in treeCommand:
+        treeLines.append(str(line))
+    tree = '\n'.join(treeLines)
+    readme.initialize(location.split('/')[-2], tree)
 
 
 if __name__ == '__main__':
--- struct-add/struct-add/readme.py
+++ struct-add/struct-add/readme.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 import os
 
 
@@ -5,7 +6,7 @@ def initializeMain():
     '''Initialize the main README.md for either language or framework
     '''
     formal_name = input(
-        "\nWhat is the formal name (put in main project README)?:\n\n")
+        "\nWhat is the formal name (put in main project README, e.g. 🎯 [Dart](https://dart.dev/))?:\n\n")
     template = f'''# {formal_name}
 
 | 🏗️ Structure Name | 📚 Description |
@@ -16,7 +17,7 @@ def initializeMain():
         readme.write(template)
 
 
-def initialize(structure_name, tree, folders):
+def initialize(structure_name, tree):
     '''Initialize the struct.md file
 
     Arguments:
@@ -30,10 +31,7 @@ def initialize(structure_name, tree, folders):
 {tree}
 ```
 '''
-    for folder in folders:
-        template = template + \
-            f'\n\n#`{folder}`\n\nA short description of what is in this folder'
-    with open('./struct.md', 'w') as struct:
+    with open('./README.md', 'w') as struct:
         struct.write(template.format(structure_name, tree))
 
 
@@ -45,14 +43,39 @@ def add_to_toc(structure_name):
     '''
     with open('../README.md') as readme:
         lines = list(readme.readlines())
-    print(type(lines))
     description = input(
         '\nWhat is the description of the structure you are adding (1 - 2 sentences)?:\n\n')
     updated_lines = []
     for line in lines:
-        if line.strip() == '<!--END OF TOC, DO NOT REMOVE-->' and f'| [{structure_name}](./{structure_name}) | {description} |\n' not in lines:
+        if line.strip() == '<!--END OF TOC, DO NOT REMOVE-->':
             updated_lines.append(
                 f'| [{structure_name}](./{structure_name}) | {description} |\n')
         updated_lines.append(line)
     with open('../README.md', 'w') as readme:
         readme.writelines(updated_lines)
+
+
+def tree(dir_path: Path, prefix: str = ''):
+    """A recursive generator, given a directory Path object
+    will yield a visual tree structure line by line
+    with each line prefixed by the same characters
+
+    Gotten from https://stackoverflow.com/questions/9727673/list-directory-tree-structure-in-python
+    Thank you Aaron Hall!
+
+    """
+    # prefix components:
+    space = '    '
+    branch = '│   '
+    # pointers:
+    tee = '├── '
+    last = '└── '
+    contents = list(dir_path.iterdir())
+    # contents each get pointers that are ├── with a final └── :
+    pointers = [tee] * (len(contents) - 1) + [last]
+    for pointer, path in zip(pointers, contents):
+        yield prefix + pointer + path.name
+        if path.is_dir():  # extend the prefix and recurse:
+            extension = branch if pointer == tee else space
+            # i.e. space because last, └── , above so no more |
+            yield from tree(path, prefix=prefix+extension)
